726bf762aa8ded6fe8e4616af634675482124c49
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2015-03-29 15:05:05 +0530

    Added the STL powered heap sort

---
+++ Introduction_to_Alogrithm/heap_sort_STL.cpp
@@ -0,0 +1,15 @@
+# include <algorithm>
+# include <iostream>
+using namespace std;
+int main()
+{
+	vector<int> arr = {19, 17, 16, 12, 9, 15, 1, 2, 11, 7, 3, 10, 14};
+	cout<<"Before sorting : ";
+	for(auto i : arr) cout<<i<<" ";
+	cout<<endl;
+	make_heap(begin(arr),end(arr));
+	sort_heap(begin(arr),end(arr));
+	cout<<"After sorting : ";
+	for(auto i : arr) cout<<i<<" ";
+}
+
